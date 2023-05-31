1a4cf9e1e4600de22c5f43cd060e2c8fd01169cc
Vairn <69882482+AdamENESS@users.noreply.github.com>
2023-05-31 11:59:26 +1000

    Add git attributes, ignore files and add CMakeLists.txt
    This commit adds a .gitattributes file to auto-detect text files and perform LF normalization. It also adds a .gitIgnore file with various ignored files such as prerequisites, compiled object files, precompiled headers, compiled dynamic libraries, Fortran module files, compiled static libraries and executables. Additionally, it includes the CMakeLists.txt file that sets the project name and compiles definitions for IMGUI

---
+++ src/examples/example_glfw_opengl3/main.cpp
@@ -0,0 +1,135 @@
+#include <GLFW/glfw3.h>
+#define IMGUI_DEFINE_MATH_OPERATORS
+#define IMGUI_DEFINE_MATH_OPERATORS_IMPLEMENTED
+#include "imgui_internal.h"
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+
+#ifndef DEBUG
+#pragma comment(linker, "/SUBSYSTEM:Windows /ENTRY:mainCRTStartup")
+#endif
+
+void renderGUI()
+{
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    // Add your GUI elements here
+
+    // Docking
+    static bool dockspaceOpen = true;
+    static bool opt_fullscreen_persistant = true;
+    bool opt_fullscreen = opt_fullscreen_persistant;
+    static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+    ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+    if (opt_fullscreen)
+    {
+        const ImGuiViewport* viewport = ImGui::GetMainViewport();
+        ImGui::SetNextWindowPos(viewport->Pos);
+        ImGui::SetNextWindowSize(viewport->Size);
+        ImGui::SetNextWindowViewport(viewport->ID);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+        window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+        window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+    }
+
+    if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+        window_flags |= ImGuiWindowFlags_NoBackground;
+
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+    ImGui::Begin("DockSpace Demo", &dockspaceOpen, window_flags);
+    ImGui::PopStyleVar();
+
+    if (opt_fullscreen)
+        ImGui::PopStyleVar(2);
+
+    // DockSpace
+    ImGuiIO& io = ImGui::GetIO();
+    if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable)
+    {
+        ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+        ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+    }
+
+    // Menu bar
+    if (ImGui::BeginMenuBar())
+    {
+        if (ImGui::BeginMenu("File"))
+        {
+            // Add menu items here
+
+            ImGui::EndMenu();
+        }
+
+        // Add more menu items here
+
+        ImGui::EndMenuBar();
+    }
+
+    // Rendering
+    ImGui::End();
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+int main()
+{
+    // Initialize GLFW
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create a GLFW window
+    GLFWwindow* window = glfwCreateWindow(800, 600, "ENESS Mapper", nullptr, nullptr);
+    if (window == nullptr)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    (void)io;
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer bindings
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+    // Enable docking
+    ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    ImGui::GetStyle().WindowRounding = 0.0f;
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+
+        // Start the ImGui frame and render GUI
+        renderGUI();
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+    }
+
+    // Cleanup
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwTerminate();
+
+    return 0;
+}
---
+++ user_guides/Lumes_Scheduler.md
@@ -0,0 +1,38 @@
+# User Guide
+> ENESS Lumes Scheduler
+
+Pictured below is what the scheduler looks like while it is running:
+
+![/Picture-01](/user_guides/Lumes_Scheduler_img/image5.png)
+
+At the top of the window you can see if the scheduler has detected a running instance of the Lumes wall software. Pressing the “Restart Lumes” button will end the current instance of the software and relaunch it.
+
+![/Picture-02](/user_guides/Lumes_Scheduler_img/image6.png)
+
+1. The scheduler controls the time of day the content will start and stop running. These are set with the `Starting Time`, and `Ending Time` combo boxes. The left box sets the Hour, the right box the minutes.
+2. The current running content, is listed on the scheduler as well, as you can see it is currently set to be running the `New England Winter` scene.
+3. The Time is set in 24 Hour format. The default settings for the software is to start at 6:00am and end at 10:00pm.
+
+![/Picture-03](/user_guides/Lumes_Scheduler_img/image2.png)
+
+4. `Load Schedule` loads in the last saved schedule, this can be usefull if you want to go back to the values as they were set before the software was loaded, or the last time they were saved.
+5. `Save Schedule` saves the changes you've made to the current schedule, this includes the `Starting Time`, and `Ending Time`.
+6. `Add Schedule Item` adds a new item to the schedule list.
+
+![/Picture-04](/user_guides/Lumes_Scheduler_img/image1.png)
+
+7. Above you can see the current setup schedule. There are currently 12 items, one for each month of the year. Each item also corresponds to one of the `Content Scenes` inside the Lumes software. There are 12 different content scenes you have been provided with.
+8. The currently active schedule is highlighted, indicated by the word `ACTIVE` behind it.
+9. Clicking on the arrow on the left of a schedule item expands it out to the following window:
+
+![/Picture-05](/user_guides/Lumes_Scheduler_img/image4.png)
+
+10. Here you are able to change the dates that the content runs from and to, and which content will be running between these times. 
+11. Changing the program of the currently active schedule item, will also update the content instantly currently running on the Lumes Wall.
+
+![/Picture-06](/user_guides/Lumes_Scheduler_img/image3.png)
+
+12. Clicking on the `down arrow` next to the start or end date will make a `Date Selection` popup appear. Here you are able to set the start and end day of the selected content.
+
+> **Note** If the end date of the content is set after the starting date of the next item in the list it will have precedence over any content following it.
+I.E: If you changed the end date of the first content, “New England Winter” to the 3rd of Feburary, then it will run until the 3rd of Feburary, and the second set of content will not start until the 4th of Feburary, even though its start date is set to be the 1st of Feburary.
